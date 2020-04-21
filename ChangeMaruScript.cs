94a4ea03f24946471615178b69f0f9c57d34f05b
tunakan06 <kombe20@gmail.com>
2020-04-21 10:35:33 +0900

    Add files via upload

---
+++ ChangeMaruScript.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeMaruScript : MonoBehaviour
+{
+    public RandomMaruScript randomMaruScript;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnMouseOver()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            gameObject.GetComponent<Renderer>().material.color = Color.yellow;
+
+        }
+        if (Input.GetMouseButtonUp(0))
+        {
+            gameObject.GetComponent<Renderer>().material.color = Color.white;
+            ObjectDestroy();
+        }
+    }
+
+    //マウスが離れたとき
+    private void OnMouseExit()
+    {
+        gameObject.GetComponent<Renderer>().material.color = Color.white;
+    }
+
+    //オブジェクトが衝突したとき
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        if (collision.collider.tag == this.tag)
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                gameObject.GetComponent<Renderer>().material.color = Color.yellow;
+
+            }
+            if (Input.GetMouseButtonUp(0))
+            {
+                gameObject.GetComponent<Renderer>().material.color = Color.white;
+                ObjectDestroy();
+            }
+        }
+    }
+
+    private void ObjectDestroy()
+    {
+        Destroy(gameObject);
+        randomMaruScript.maruCount--;
+        Debug.Log(randomMaruScript.maruCount);
+    }
+
+}
---
+++ RandomMaruScript.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomMaruScript : MonoBehaviour
+{
+    //　出現させる丸を入れておく
+    [SerializeField] GameObject[] maru;
+    public int maruCount;
+    int maxMaruCount = 40;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        maruCount = 0;
+        //maru = GameObject.FindGameObjectsWithTag("maru");
+        //maruCount = maru.Length;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if ( maruCount <= maxMaruCount ) {
+            AppearMaru();
+        }
+
+    }
+
+    void AppearMaru()
+    {
+        int randomValue = Random.Range(0, maru.Length);
+        float randomValueX = Random.Range(-2.5f, 2.5f);
+        float randomValueY = Random.Range( 4.5f, 5.5f);
+
+        GameObject.Instantiate( maru[randomValue], new Vector3(randomValueX, randomValueY), Quaternion.Euler(0f, 0f, Random.value * 360f));
+        maruCount++;
+        Debug.Log(maruCount);
+    }
+
+}
